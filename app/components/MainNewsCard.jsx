f21534aa35193e6b0e9810b201bdde0c207b4546
lichpark <lichpark0@hanmail.net>
2024-12-02 17:50:15 +0900

    port ing

--- app/components/MainNewsCard.jsx
+++ app/components/MainNewsCard.jsx
@@ -11,9 +11,17 @@ const MainNewsCard = (props) => {
   //   }, []);
   function fadeDesc(e) {
     const container = e.currentTarget.querySelector(".content");
-    container.style.transform = "translateY(-60%)";
-    container.style.backgroundColor = "#13161b";
-    container.style.opacity = "0.6";
+    const parent = container.parentElement; // 부모 요소 가져오기
+
+    //동적 높이 계산
+    const parentHeight = parent.offsetHeight;
+    const containerHeight = container.offsetHeight;
+    const translateY = -(parentHeight - containerHeight);
+    console.log(parentHeight);
+    console.log(containerHeight);
+    console.log(translateY);
+    container.style.transform = `translateY(${translateY}px)`;
+    container.style.backgroundColor = "rgba(0,0,0,0.5)";
     container.style.transition = "transform 0.5s ease";
   }
 
@@ -26,7 +34,7 @@ const MainNewsCard = (props) => {
   return (
     <Link href="" style={{ width: "28%" }}>
       <div
-        className="h-66"
+        className="h-40"
         style={{
           width: "100%",
           backgroundImage: `url(${props.src})`,
@@ -36,7 +44,7 @@ const MainNewsCard = (props) => {
           display: "flex",
           alignItems: "flex-end",
           borderBottom: "2px solid gray",
-          position: "relative",
+
           overflow: "hidden",
         }}
         onMouseOver={(e) => fadeDesc(e)}
@@ -45,23 +53,29 @@ const MainNewsCard = (props) => {
         <div
           className="content"
           style={{
-            position: "relative",
             transform: "translateY(0)",
             transition: "transform 0.3s ease",
             bottom: "-100px",
+            width: "100%",
           }}
         >
           <div style={{ color: "rgba(255,255,255,.7)", fontSize: "16px" }}>
             {props.date}
           </div>
-          <div style={{ color: "#fff", fontSize: "24px" }}>{props.Title}</div>
+          <div
+            style={{ color: "#fff", fontSize: "24px" }}
+            className="newsTitle"
+          >
+            {props.Title}
+          </div>
           <p
             style={{
-              color: "#fff",
+              color: " rgba(255, 255, 255, 0.7)",
               fontSize: "14px",
-              display: "block", // 항상 표시
+              display: "none", // 항상 표시
               marginTop: "10px",
               overflow: "hidden",
+              height: "60px",
             }}
           >
             {props.desc}
